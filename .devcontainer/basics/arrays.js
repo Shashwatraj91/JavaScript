b31a78687583e921b7d7e7c4a4c90e503092c7ab
Shashwat raj <76648622+Shashwatraj91@users.noreply.github.com>
2024-07-09 09:03:11 +0000

    basics of array

---
+++ .devcontainer/basics/arrays.js
@@ -0,0 +1,9 @@
+const myArr = [0,1,2,3,4,true,"Shashwat"] // array can be of different types 
+    //and can be resizable after declaring the array
+    //shallow copy means the copy will reference to the same memory location(done using heap)
+    // Deep copy means the copy will not point to the same memory location(done using stack)
+// Arrays can be decalared by various methods:-
+//1.Method
+const arr1=["shashwat","ram","shyam"]
+//2.Method
+const arr2=new Array(1,23,454,7,86,90)
